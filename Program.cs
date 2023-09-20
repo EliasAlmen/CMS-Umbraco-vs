a00457572142bf8cf8cff305009d4ead9b7de060
EliasAlmen <almen.elias@gmail.com>
2023-09-20 12:37:03 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+namespace EC07_CMS_Umbraco_vs
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+            => CreateHostBuilder(args)
+                .Build()
+                .Run();
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureUmbracoDefaults()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStaticWebAssets();
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
